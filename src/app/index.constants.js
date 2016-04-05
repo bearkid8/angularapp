e4bccd6a4f0eca6b26d08d9eb731c43676ec08b0
Mike K <johndoe@example.com>
2016-04-05 14:21:08 +0300

    initial commit after yeoman gulp-angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularapp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularapp', ['restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
